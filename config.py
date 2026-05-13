f1ae8b98f593cf96d00e7c635c0e5de37aee8ddc
giorgiovg3003 <giorgiovg3003@gmail.com>
2026-05-13 20:29:01 +0200

    creation: config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+# Set PROJECT_PATH to your local project directory before running any notebook.
+# Example (local): PROJECT_PATH = '/home/user/pipeline_explicabilidad_medica_CNN'
+
+PROJECT_PATH = ''

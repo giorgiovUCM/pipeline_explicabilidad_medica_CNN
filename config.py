948becf8590ad43de6c42679639e22503c3bd5e0
giorgiovg3003 <giorgiovg3003@gmail.com>
2026-05-14 16:57:13 +0200

    From project_path to dir

--- config.py
+++ config.py
@@ -2,11 +2,15 @@
 # Set PROJECT_PATH to your local project directory before running any notebook.
 # Example (local): PROJECT_PATH = '/home/user/pipeline_explicabilidad_medica_CNN'
 
-PROJECT_PATH = ''
+PROJECT_DIR = ''
 
-DATASET_DIR   = f'{PROJECT_PATH}/filtered_dataset'    # (filtered images + CSVs)
-KAGGLE_DIR      = f'{PROJECT_PATH}/dataset'           # temporal download of the original dataset to then be filtered and moved to DATASET_DIR in 01_dataset_import.ipynb
-MODELS_DIR    = f'{PROJECT_PATH}/models'              # saved models
-MAPS_DIR      = f'{PROJECT_PATH}/maps'                # Grad-CAM maps
+DATASET_PATH   = f'{PROJECT_DIR}/filtered_dataset.zip'            # (filtered images + CSVs)
+DATASET_DIR    = f'{PROJECT_DIR}/filtered_dataset'                        # filtered dataset (uncompressed)
+KAGGLE_DIR     = f'{PROJECT_DIR}/dataset'                        # temporal download of the original dataset to then be filtered and moved to DATASET_DIR in 01_dataset_import.ipynb
+
+MODELS_DIR     = f'{PROJECT_DIR}/models'                           # saved models
+
+MAPS_PATH      = f'{PROJECT_DIR}/grad_cam_maps.zip'               # Grad-CAM maps
+MAPS_DIR       = f'{PROJECT_DIR}/grad_cam_maps'               # Grad-CAM maps (uncompressed)
 
 TAGS = ['aneurysm', 'cardiomegaly']
